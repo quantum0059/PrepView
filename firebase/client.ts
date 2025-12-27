ad642e0974fe235acf18187d82676a640d003bed
quantum0059 <157235980+quantum0059@users.noreply.github.com>
2025-12-27 19:42:05 +0530

    Remove Firebase config from client.ts
    Removed sensitive Firebase configuration details.

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,13 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDuSVSPqLyQenGALHJzvM473qDyiT7jWZM",
-  authDomain: "prepwise-1d3ac.firebaseapp.com",
-  projectId: "prepwise-1d3ac",
-  storageBucket: "prepwise-1d3ac.firebasestorage.app",
-  messagingSenderId: "988336814532",
-  appId: "1:988336814532:web:ea970b659eb0a4548e8c91",
-  measurementId: "G-C54VE4KFJM"
+
 };
 
 // Initialize Firebase
