33d5d92aa747bd7fbb4e1a5201ec1d2ee1d6efa5
quantum0059 <shivamsharma.anva@gmail.com>
2026-01-06 00:08:58 +0530

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import {useRouter} from "next/navigation";
 import { vapi } from "@/lib/vapi.sdk"
 import {interviewer} from "@/constants"
+import { createFeedback } from '@/lib/actions/general.action';
 
 
 enum CallStatus{
@@ -74,6 +75,19 @@ const Agent = ({userName,
   
   const handleGenerateFeedback = async (messages: SavedMessage[]) => {
     console.log('Generated feedback here. ');
+
+    const {success, feedbackId: id} = await createFeedback({
+      interviewId: interviewId!,
+      userId: userId!,
+      transcript: messages
+    })
+
+    if(success && id){
+      router.push(`/interview/${interviewId}/feedback`);
+    }else{
+      console.log('Error saving feedback')
+      router.push('/');
+    }
 
   }
 
